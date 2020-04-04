091228f005c589a1093cfdf9defa36113924482a
Elias Mendes <elias.mendes@sc.senai.br>
2020-04-04 07:57:31 -0300

    adicionado arquivos web

---
+++ web/build/precache-manifest.c57a8e367159941fe0e19f236ce67e93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c2b3c85811fb8c792a51feff04be5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b96f85706ca239dbad",
+    "url": "/static/js/2.794dd37e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.794dd37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c57ddc5d14c700b7a1",
+    "url": "/static/js/main.cfcb96da.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
