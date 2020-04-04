162f72eae7bcde31355be158abceaf7ae7235300
Elias Mendes <elias.mendes@sc.senai.br>
2020-04-04 07:48:00 -0300

    ajustes

--- frontend2/build/static/js/main.cfcb96da.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),o=t.n(r),a=t(2);var c=function(){return o.a.createElement("h1",null,"Hello ELias!")};t.n(a).a.render(o.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.cfcb96da.chunk.js.map
