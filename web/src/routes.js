20167baf2fdfc446ef08c646ab8c243666877a27
Elias Mendes <elias.mendes@sc.senai.br>
2020-04-06 08:07:20 -0300

    Adicionado nossas modificações

--- web/src/routes.js
+++ web/src/routes.js
@@ -2,11 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Logon from "./pages/Logon";
+import Register from "./pages/Register";
 
 export default function Routes() {
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={Logon} />
-    </Switch>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
